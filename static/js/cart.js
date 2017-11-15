bce87aff53a5a46f69fe7b84ac6bfd7fc4da834c
GHOST <xumin@lxs-MacBook-Air.local>
2017-11-15 11:25:30 +0800

    -

--- static/js/cart.js
+++ static/js/cart.js
@@ -101,27 +101,38 @@ $('#tobuy').on('click',function(){
                   html : 
                         '<form id="cartfrom">'+
                         '<input name="recipients" class="swal2-input" autofocus placeholder="Name" id="name"/>'+
-                        '<input name="phone" class="swal2-input" placeholder="Mobile" id="phone"/>'+
+                        '<input name="phone" class="swal2-input" placeholder="Mobile" id="mobile"/>'+
                         '<input name="email" class="swal2-input" placeholder="Email" id="email"/>'+
                         '<textarea name="address" class="swal2-textarea" placeholder="Address" style="height: 66px; resize: none;" id="address"></textarea>'+
+                        '<p style="color: red; display: none;" id="error-msg">Please ensure all fields are filled in before submitting order.</p>'+
                         '<p class="text-l"><strong>Please Select a Delivery Option:</strong></p>'+
                         '<div class="text-l"><input type="radio" value="Express Delivery (20 rmb delivery fee)" name="type" checked="checked" style="margin-right: 10px;"/>Express Delivery (20 rmb delivery fee)</div>'+
                         '<div class="text-l"><input type="radio" value="Self-Pickup from Z&B Studio" name="type" style="margin-right: 10px;"/>Self-Pickup from Z&B Studio</div>'+
                         '</form>',
-                  inputValidator : function(value) {
-                        return new Promise(function(resolve,reject) {
-                              if(value) {
-                                    resolve();
-                              }else{
-                                    reject('Please ensure all fields are filled in before submitting order.');
-                              }
-                        })
-                  }
+
+                  preConfirm: function() {
+                        var name = $('#name').val(),
+                        email = $('#email').val(),
+                        mobile = $('#mobile').val(),
+                        address = $('#address').val(),
+                        phoneReg = /(^[0-9]{3,4}-[0-9]{3,8}$)|(^[0-9]{3,8}$)|(^[0−9]3,4[0-9]{3,8}$)|(^0{0,1}1[0-9]{10}$)/,
+                        reg = /^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
+
+                        if(name == '' || mobile == ''){
+                              $('#error-msg').css('display','block');
+                        }else if(!reg.test(email) || email == ''){
+                              $('#error-msg').css('display','block');
+                        }else if(!phoneReg.test(mobile) || mobile == '') { 
+                              $('#error-msg').css('display','block');
+                        }else{
+                              return new Promise(function(resolve) {
+                                   resolve([name,email,mobile,address]);
+                              });
+                        }
+                  },
             }).then(function(result) {
-                  if(result) {
-            // .then(function(isConfirm) {
-            //       if(isConfirm === true) {
-                        var address = $('#cartfrom').serialize();
+                   if(result) {
+                        var address = $('#cartfrom').serialize();                        
 
                         $.ajax({
                               cache : false,
